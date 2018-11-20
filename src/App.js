e73b65eeb961d6a06227d7615178cb11742e0e6d
Carlos Andrade <candrade@andradesoft.com>
2018-11-20 14:05:05 -0500

    Front-end template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './components/NavBar'
+import Main from './components/Main'
+
+const App = () => (
+      <div className='app'>
+        <NavBar />
+        <Main />
+      </div>
+    );
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+
+const Home = ({ className }) => {
+    return (
+
+    	<div className="container text-center">
+    		<h1>Welcome to ShareBooks</h1>
+    	</div>
+        
+    );
+};
+
+
+
+export default Home;
